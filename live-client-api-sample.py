edf5988e7d1011ae67e07e278efd8f61af60da55
Samuel Eliot Nathanson <sen52@drexel.edu>
2020-04-20 23:59:32 -0400

    Added live client API sample

---
+++ live-client-api-sample.py
@@ -0,0 +1,13 @@
+import json
+import requests
+
+host_port = "https://127.0.0.1:2999/"
+get_summoner_name_endpoint = "liveclientdata/activeplayername"
+get_active_player_list_endpoint = "liveclientdata/playerlist"
+
+
+
+
+
+
+
