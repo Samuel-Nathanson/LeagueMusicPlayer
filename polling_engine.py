ce4611144be986add0941dce4e3d2ca3e00a397f
Picklegunner <rvs20hw@gmail.com>
2020-04-22 21:31:09 -0400

    Updated polling engine on feedback. PollingEngine encompassed as a class

--- polling_engine.py
+++ polling_engine.py
@@ -3,56 +3,81 @@ import time
 import json
 
 # Simple polling of web server to get back champion name when game starts
+class PollingEngine():
 
-host_port = "https://127.0.0.1:2999/"
-get_summoner_name_endpoint = "liveclientdata/activeplayername"
-get_active_player_list_endpoint = "liveclientdata/playerlist"
+    def __init__(self, verify_ssl = True, interval = 5):
+        self.host_port = "https://127.0.0.1:2999/"
+        self.get_summoner_name_endpoint = "liveclientdata/activeplayername"
+        self.get_active_player_list_endpoint = "liveclientdata/playerlist"
 
-def check_in_game():
-    
-    # Make request to webserver
-    try:
-        response = requests.get(host_port + get_summoner_name_endpoint, verify = False)
-        if response.status_code == 200:
-            summoner_name = response.json()
-        elif response.status_code == 404:
-            print("Response returned HTTP 404 - In loading screen") # TODO potentially try again sooner or see if other endpoints are available
-            return
-        else:
-            print("Unexpected response!")
-            print(json.dumps(response.json()))
-            return
-    except requests.exceptions.RequestException as e:
-        print("RequestException - Likely out of game")
-        return
+        self.interval = interval
+        self.verify_ssl = verify_ssl
 
-    # Get champion name
-    try:
-        response = requests.get(host_port + get_active_player_list_endpoint, verify = False)
-        if response.status_code != 200:
-            print(json.dumps(response.json()))
-        else:
-            for player in response.json():
-                if player["summonerName"] == summoner_name:
-                    print(player["championName"])
-    except requests.exceptions.RequestException as e:
-        print(e) # This should probably never happen
-        return
+    def get_active_champion(self):
 
-# Poll server on some interval
-def polling_loop(interval):
-    while True:
-        check_in_game()
-        time.sleep(interval)
+        summoner_name = self.get_summoner_name()
+        if summoner_name == None:
+            return None
 
-# Supress SSL #TODO enable SSL
-requests.packages.urllib3.disable_warnings()
+        # Make request to webserver for champion name
+        try:
+            response = requests.get(self.host_port + self.get_active_player_list_endpoint, verify = self.verify_ssl)
+            if response.status_code != 200:
+                print(json.dumps(response.json()))
+            else:
+                for player in response.json():
+                    if player["summonerName"] == summoner_name:
+                        return player["championName"]
+        except requests.exceptions.RequestException as e:
+            print(e) # This should probably never happen
+        
+        return None
 
-polling_loop(5)
+    # Make request to webserver for summoner name
+    # Do not cache summoner_name to handle account switching
+    def get_summoner_name(self):
 
+        summoner_name = None
+        
+        try:
+            response = requests.get(self.host_port + self.get_summoner_name_endpoint, verify = self.verify_ssl)
+            if response.status_code == 200:
+                summoner_name = response.json()
+            elif response.status_code == 404:
+                print("Response returned HTTP 404 - In loading screen") # TODO potentially try again sooner or see if other endpoints are available
+            else:
+                print("Unexpected response!")
+                print(json.dumps(response.json()))
+        except requests.exceptions.RequestException as e:
+            print("RequestException - Likely out of game")
+
+        return summoner_name
+
+    # Poll server on some interval
+    def poll_loop(self):
+        while True:
+            print(self.get_active_champion())
+            time.sleep(self.interval)
+
+
+
+if __name__ == "__main__":
+
+    # Supress SSL #TODO enable SSL
+    requests.packages.urllib3.disable_warnings()
+
+    # init polling engine
+    engine = PollingEngine(verify_ssl = False)
+    engine.poll_loop()
 
 """
  Findings:
  Connections will be refused while league client is closed
  In loading screen endpoints will return 404s
+
+ TODO
+ Check sooner if user is in loading screen
+ Implement async functions
+ See if SSL works on windows machines
+ Maybe implement logger
 """
